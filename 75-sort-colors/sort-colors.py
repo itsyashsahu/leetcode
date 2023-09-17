4a0a1c95b558f56a072b5da345863ee6d5a0b5ed
Yash Sahu <itsyashsahu@gmail.com>
2023-09-17 12:05:45 +0530

    Time: 35 ms (91.06%) | Memory: 16.3 MB (67.34%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,24 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        cnt0,cnt1,cnt2=0,0,0
-        for num in nums:
-            if num==0:
+        cnt0,cnt1,cnt2 = 0,0,0
+
+        for n in nums:
+            if n ==0:
                 cnt0+=1
-            elif num==1:
+            elif n ==1:
                 cnt1+=1
-            elif num==2:
+            elif n ==2:
                 cnt2+=1
-        i = 0
-        while cnt0>0:
-            nums[i]=0
-            cnt0-=1
-            i+=1
-        while cnt1>0:
-            nums[i]=1
-            cnt1-=1
-            i+=1
-        while cnt2>0:
-            nums[i]=2
-            cnt2-=1
-            i+=1
+        
+        for i in range(len(nums)):
+            if cnt0:
+                nums[i]=0
+                cnt0-=1
+            elif cnt1:
+                nums[i]=1
+                cnt1-=1
+            elif cnt2:
+                nums[i]=2
+                cnt2-=1
