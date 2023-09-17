39154d724718be6ac3e782f58f70142a3acc860c
Yash Sahu <itsyashsahu@gmail.com>
2023-09-17 11:58:20 +0530

    Time: 49 ms (53.86%) | Memory: 16.3 MB (71.22%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -3,19 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = len(nums)
-        idx = -1 
-        for i in range(l-1,0,-1):
-            if nums[i] > nums[i-1]:
+        n = len(nums)
+        idx = -1
+        for i in range(n-1,0,-1):
+            # print("i -- ",i)
+            if nums[i]>nums[i-1]:
+                # print(f"milla -{i}")
                 idx = i-1
                 break
-        if idx != -1:
-            for i in range(l-1,idx,-1):
-                if nums[idx]< nums[i]:
-                    nums[idx],nums[i] = nums[i],nums[idx]
-                    break
-            nums[idx+1:] = sorted(nums[idx+1:])
-        else:
+        # print(idx)
+        if idx == -1:
             nums.reverse()
-
+        else:
+            for i in range(n-1,0,-1):
+                if nums[idx]<nums[i]:
+                    nums[i],nums[idx] = nums[idx],nums[i]
+                    nums[idx+1:] = sorted(nums[idx+1:])
+                    break
+            
         
