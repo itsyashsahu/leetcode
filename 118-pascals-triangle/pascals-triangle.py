deeb0f3b92bde4880cb6d2b1750f228a70c44de9
Yash Sahu <itsyashsahu@gmail.com>
2023-09-28 16:16:48 +0530

    Time: 42 ms (47.15%) | Memory: 16.1 MB (99.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -2,12 +2,11 @@ class Solution:
     def generate(self, n: int) -> List[List[int]]:
         ans = []
         for l in range(1,n+1):
-            temp = []
+            temp = [1]
             t = 1
-            temp.append(1)
-            for r in range(1,l):
-                t =(t*(l-r))//r
-                # t = t//r 
+            for r in range( 1, l):
+                t = (t*(l-r))//r
                 temp.append(t)
+
             ans.append(temp)
         return ans
