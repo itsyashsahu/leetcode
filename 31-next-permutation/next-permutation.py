75529984d5e5189f8c5343e56cfb06cf4427c53e
Yash Sahu <itsyashsahu@gmail.com>
2023-09-28 16:39:40 +0530

    Time: 54 ms (15.19%) | Memory: 16.4 MB (30.48%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -6,19 +6,16 @@ class Solution:
         n = len(nums)
         idx = -1
         for i in range(n-1,0,-1):
-            # print("i -- ",i)
             if nums[i]>nums[i-1]:
-                # print(f"milla -{i}")
                 idx = i-1
                 break
-        # print(idx)
         if idx == -1:
             nums.reverse()
         else:
             for i in range(n-1,0,-1):
                 if nums[idx]<nums[i]:
                     nums[i],nums[idx] = nums[idx],nums[i]
-                    nums[idx+1:] = sorted(nums[idx+1:])
+                    nums[idx+1:] = reversed(nums[idx+1:])
                     break
             
         
