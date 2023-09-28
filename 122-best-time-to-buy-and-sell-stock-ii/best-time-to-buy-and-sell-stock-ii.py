cf65206dd2780169cba6ee38ba014ea706bfdc26
Yash Sahu <itsyashsahu@gmail.com>
2023-09-28 19:27:27 +0530

    Time: 63 ms (64.20%) | Memory: 17.8 MB (52.05%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices)<2:
+            return 0
+        temp = []
+        for i in range(len(prices)-1):
+            temp.append(prices[i+1]-prices[i])
+        
+        def lcs(arr):
+            # maxi = max(arr)
+            sumi = 0
+            # if maxi <0:
+            #     return maxi
+            for i in range(len(arr)):
+                if arr[i]>=0:
+                    sumi+=arr[i]
+            return sumi
+        ans = lcs(temp)
+        if ans<0:
+            return 0
+        return ans
